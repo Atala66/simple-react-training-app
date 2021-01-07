3f2514fb7a87797f45d550555db6b329e69802b6
Guiomar <guioguguloxilox@gmail.com>
2021-01-07 09:20:59 +0100

    for loop of images

--- src/components/Content/home.jsx
+++ src/components/Content/home.jsx
@@ -1,16 +1,50 @@
-import React from 'react';
-import cat_img from '../../images/cat_img.png';
-import home_styles from '../../styles/home_styles.scss';
+import React from "react";
+import cat_one from '../../images/cats/cat_one.png';
+import cat_two from '../../images/cats/cat_two.png';
+import home_styles from "../../styles/home_styles.scss";
 
+export const CatImages = [
+  {
+    id: 1,
+    name: "cat_one",
+    src: cat_one,
+  },
+  {
+    id: 2,
+    name: "cat_one",
+    src: cat_one,
+  },
+  {
+    id: 3,
+    name: "cat_one",
+    src: cat_one,
+  },
+  {
+    id: 4,
+    name: "cat_one",
+    src: cat_one,
+  },
+  {
+    id: 5,
+    name: "cat_one",
+    src: cat_one,
+  },
+  {
+    id: 6,
+    name: "cat_one",
+    src: cat_one,
+  },
+];
 
 const Home = () => {
-		return(
-			<div className="main-content"> <img className="cat-img" src={cat_img} alt="cat"/>  </div>
-		);
-	
-
+  return (
+    <div className="main-content">
+		{ CatImages.map(cat => {
+			return  <img key={cat.id} src={cat.src} alt="Cat_image" className="cat-img"/>;
+		})}
+     
+    </div>
+  );
 };
-
-
 
 export default Home;
